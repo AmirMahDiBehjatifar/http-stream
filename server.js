a5c16d28535ae07a5de8995040732a5ba18a6d2d
AmirMehDiBehjatifar <Mrbehjatifara30@gmail.com>
2024-02-05 09:59:02 +0330

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const { createReadStream, statSync } = require("fs")
+const fileName = "./nature.mp4"
+
+http.createServer(async (req, res) => {
+    const readStream = createReadStream(fileName);
+    const { size } = statSync(fileName)
+    console.log(size);
+    const range = req.headers.range
+    if (range) {
+        let [start, end] = range.replace(/bytes=/, '').split("-")
+        start = parseInt(start, 10);
+        end = end ? parseInt(start, 10) : size - 1;
+        console.log(start, end);
+        res.writeHead(206, {
+            'Content-Range': `bytes ${start}-${end}/${size}`,
+            'Accept-Range': 'bytes',
+            'Content-Length': (start, end) + 1,
+            'Content-Type': 'video/mp4'
+            
+        })
+        createReadStream(fileName,{start,end}).pipe(res)
+    } else {
+        res.writeHead(200, {
+             'Content-Length': size,
+             'Content-Type': 'video/mp4'
+             })
+        readStream.pipe(res)
+    }
+
+}).listen(3000)
